887906bbca1caf7abca449aa046dfb41d68fbd3d
jidraph kimachia <jidraphkimachia1@gmail.com>
2022-04-19 19:47:46 +0300

    service providers

--- GitViewMe/src/app/services/github/github.service.ts
+++ GitViewMe/src/app/services/github/github.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 import { User } from './../../models/user';
 import { Repo } from 'src/app/models/repo';
@@ -11,8 +11,21 @@ export class GithubService {
 
   private username: string = "Jidraph1";
 
-  constructor(private httpClient: HttpClient) { 
 
-    getUser():Promise
+  constructor(private httpClient: HttpClient) {
+   }
+
+   getUser(): Promise<User>{
+    return this.httpClient.get<User>(`https://api.github.com/users/${this.username}`)
+    .toPromise();
+  }
+
+  getRepos(): Promise<Repo[]>{
+    return this.httpClient.get<Repo[]>('https://api.github.com/users/' + this.username+'/repos')
+    .toPromise();
+  }
+
+  updateUsername(username: string) {
+this.username = username;
   }
 }
